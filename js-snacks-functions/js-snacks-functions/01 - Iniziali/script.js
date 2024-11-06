93a46800907ea109b014e2aeab6c6388e1d67380
ritaurbani <ritaurbani@gmail.com>
2024-11-06 15:50:30 +0100

    feat: iniziali-to be reviewed

--- js-snacks-functions/js-snacks-functions/01 - Iniziali/script.js
+++ js-snacks-functions/js-snacks-functions/01 - Iniziali/script.js
@@ -6,8 +6,25 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 // Dichiara la funzione qui.
 
 
+//  Description
+//  @returns {array}
+
+
+function namesArray ( ) {
+    let letterArray = [];
+
+    for ( let i=0; i<names.length; i++){
+        const currenntItem = names[i];
+        let firstLetter = currenntItem.charAt(0)
+        letterArray.push(firstLetter)
+    } 
+    return letterArray;
+}
+
 // Invoca la funzione qui e stampa il risultato in console
 
+const arrayOfLetters = namesArray ( names)
+console.log(arrayOfLetters)
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
