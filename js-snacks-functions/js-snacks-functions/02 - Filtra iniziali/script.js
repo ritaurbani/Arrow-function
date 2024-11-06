b4e40236f394740a6e932fcf599d55459a6ab386
ritaurbani <ritaurbani@gmail.com>
2024-11-06 16:41:49 +0100

    feat: flitra iniziali - to be reviewed

--- js-snacks-functions/js-snacks-functions/02 - Filtra iniziali/script.js
+++ js-snacks-functions/js-snacks-functions/02 - Filtra iniziali/script.js
@@ -6,8 +6,28 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 // Dichiara la funzione qui.
 
 
+//  * Description
+//  * @param {array} arr
+//  * @param {string} letter
+//  * @returns {any}
+ 
+function arrayOfWords(arr, letter ) {
+    const  arrResult = [];
+    for (let i = 0; i < arr.length; i++) {
+            const currentItem = arr[i];
+            let firstLetter = currentItem.charAt(0)
+            if (letter === firstLetter) {
+                arrResult.push(currentItem)
+            }
+        } 
+
+    return arrResult; 
+}
+
+
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const risultato = arrayOfWords( names, "L")
+console.log(risultato)
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
