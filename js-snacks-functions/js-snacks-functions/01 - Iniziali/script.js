31be2df32b811cf7a1eb52ffaf740a7eef51f055
ritaurbani <ritaurbani@gmail.com>
2024-11-06 17:31:39 +0100

    feat: vowel function

--- js-snacks-functions/js-snacks-functions/01 - Iniziali/script.js
+++ js-snacks-functions/js-snacks-functions/01 - Iniziali/script.js
@@ -1,30 +1,30 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
-// const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
+const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
-// // Dichiara la funzione qui.
+// Dichiara la funzione qui.
 
 
-// //  Description
-// //  @returns {array}
+//  Description
+//  @returns {array}
 
 
-// function namesArray (arr ) {
-//     let letterArray = [];
+function namesArray (arr ) {
+    let letterArray = [];
 
-//     for ( let i=0; i<arr.length; i++){
-//         const currenntItem = arr[i];
-//         let firstLetter = currenntItem.charAt(0)
-//         letterArray.push(firstLetter)
-//     } 
-//     return letterArray;
-// }
+    for ( let i=0; i<arr.length; i++){
+        const currenntItem = arr[i];
+        let firstLetter = currenntItem.charAt(0)
+        letterArray.push(firstLetter)
+    } 
+    return letterArray;
+}
 
-// // Invoca la funzione qui e stampa il risultato in console
+// Invoca la funzione qui e stampa il risultato in console
 
-// const arrayOfLetters = namesArray ( names)
-// console.log(arrayOfLetters)
+const arrayOfLetters = namesArray ( names)
+console.log(arrayOfLetters)
 
 
-// //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+//Risultato atteso: ["A", "L", "M", "A", "G", "A"]
--- js-snacks-functions/js-snacks-functions/03 - conta vocali/script.js
+++ js-snacks-functions/js-snacks-functions/03 - conta vocali/script.js
@@ -5,6 +5,24 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+/**
+ * Description
+ * @param {string} parola
+ * @returns {number}
+ */
+function vowelNumber( parola) {
+ const newString = "";
+ const result = newString.length();
+ for (let i=0; i<parola.length;i++){
+    let currentLetter = parola[i];
+    if (currentLetter 
+ }
+ 
+
+ return result 
+}
+
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
